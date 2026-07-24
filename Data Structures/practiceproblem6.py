004165dbd022726abce6466c0ac2acffc9b9c05e
Monish-2007-hub <k2007monish@gmail.com>
2026-07-24 21:47:37 +0530

    storing subjects and marks in a dictionary

---
+++ Data Structures/practiceproblem6.py
@@ -0,0 +1,12 @@
+dict = {}
+sub = input("enter a subject: ")
+val = int(input("enter marks: ")) 
+dict.update({sub : val})
+sub = input("enter a subject: ")
+val = int(input("enter marks: ")) 
+dict.update({sub : val})
+sub = input("enter a subject: ")
+val = int(input("enter marks: ")) 
+dict.update({sub : val})
+
+print(dict)
