7357d80bbcfb7ac0decf9083a2818f47803d060f
Monish-2007-hub <k2007monish@gmail.com>
2026-07-24 11:32:33 +0530

    counter

---
+++ Data Structures/practiceproblem3.py
@@ -0,0 +1,8 @@
+# to couunt the number of students with grade A in the following tuple
+# ("C","D","A","A","B","B","A")
+
+grade = ("C","D","A","A","B","B","A")
+
+count = grade.count("A")
+
+print("No. of students with grade A: ",count)
