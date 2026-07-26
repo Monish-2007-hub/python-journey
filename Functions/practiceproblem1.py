7cbbd0615256f712aba8c688d728db0661009a75
Monish-2007-hub <k2007monish@gmail.com>
2026-07-26 21:56:47 +0530

    printing length of a list

---
+++ Functions/practiceproblem1.py
@@ -0,0 +1,8 @@
+cities = ["chennai", "chittoor", "hyderadab", "banglore"]
+heroes = ["spider-man", "ironman", "thor", "ben 10", "captain america"]
+
+def print_len(list):
+    print(len(list))
+
+print_len(cities)
+print_len(heroes)
